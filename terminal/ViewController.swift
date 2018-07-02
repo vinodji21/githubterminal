9c170625bd22046fe691568561a3796315801169
Chintalapudi Vinod <chintalapudivinod@Achiever-19.local>
2018-07-02 16:28:53 +0530

    secondcommit

--- terminal/ViewController.swift
+++ terminal/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
  var name = "Vinod"
+    
+    var name1 ="Lokesh"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
