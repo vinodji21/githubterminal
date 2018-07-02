a9cc62488b2b171600d7943a40a195cfe9fa31bb
Chintalapudi Vinod <chintalapudivinod@Achiever-19.local>
2018-07-02 17:18:21 +0530

    fifthcommit

--- terminal/ViewController.swift
+++ terminal/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
  var name = "Vinod"
     
     var name1 = "Lokesh"
+    
+    var name2 = "Vinu"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
